9c8b8d0d27f10fc92d9b62b8108f0ba234c2efc5
eric ndungutse <dav.ndungutse@gmail.com>
2023-10-25 09:08:18 +0200

    validate form

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import Input from '../../ui/Input';
 import Form from '../../ui/Form';
@@ -7,51 +9,47 @@ import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
 import { createCabin } from '../../services/apiCabins';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
-import { UNSAFE_DataRouterStateContext } from 'react-router-dom';
+import FormRow from '../../ui/FormRow';
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
+// const FormRow = styled.div`
+//   display: grid;
+//   align-items: center;
+//   grid-template-columns: 24rem 1fr 1.2fr;
+//   gap: 2.4rem;
 
-  padding: 1.2rem 0;
+//   padding: 1.2rem 0;
 
-  &:first-child {
-    padding-top: 0;
-  }
+//   &:first-child {
+//     padding-top: 0;
+//   }
 
-  &:last-child {
-    padding-bottom: 0;
-  }
+//   &:last-child {
+//     padding-bottom: 0;
+//   }
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
 
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
+//   &:has(button) {
+//     display: flex;
+//     justify-content: flex-end;
+//     gap: 1.2rem;
+//   }
+// `;
 
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+// const Error = styled.span`
+//   font-size: 1.4rem;
+//   color: var(--color-red-700);
+// `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, getValues, formState } =
+    useForm();
   const queryClient = useQueryClient();
+  const { errors } = formState;
 
-  const { isCreating: isCreating, mutate } = useMutation({
+  const { isLoading: isCreating, mutate } = useMutation({
     mutationFn: createCabin,
     onSuccess: () => {
       toast.success('Cabin uccessfully created');
@@ -70,52 +68,96 @@ function CreateCabinForm() {
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor='name'>Cabin name</Label>
-        <Input type='text' id='name' {...register('name')} />
+      <FormRow
+        label='Cabin name'
+        error={errors?.name && errors?.name?.message}>
+        <Input
+          type='text'
+          id='name'
+          {...register('name', {
+            required: 'This field is required',
+          })}
+          disabled={isCreating}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='maxCapacity'>Maximum capacity</Label>
+      <FormRow
+        label='Maximum capacity'
+        error={
+          errors['maxCapacity'] && errors['maxCapacity'].message
+        }>
         <Input
           type='number'
           id='maxCapacity'
-          {...register('maxCapacity')}
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity should atleast be 1',
+            },
+          })}
+          disabled={isCreating}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='regularPrice'>Regular price</Label>
+      <FormRow
+        label='Regular price'
+        error={
+          errors['regularProce'] && errors['regularProce'].message
+        }>
         <Input
           type='number'
           id='regularPrice'
-          {...register('regularPrice')}
+          {...register('regularPrice', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity should atleast be 1',
+            },
+          })}
+          disabled={isCreating}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='discount'>Discount</Label>
+      <FormRow
+        label='Discount'
+        error={errors?.discount && errors?.discount?.message}>
         <Input
           type='number'
           id='discount'
           defaultValue={0}
-          {...register('discount')}
+          {...register('discount', {
+            required: 'This field is required',
+            validate: (val) =>
+              val <= getValues().regularPrice ||
+              'Discount should be less thatn regular price',
+          })}
+          disabled={isCreating}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='description'>Description for website</Label>
+      <FormRow
+        label='Description for website'
+        error={
+          errors['description'] && errors['description'].message
+        }>
         <Textarea
           type='number'
           id='description'
           defaultValue=''
-          {...register('description')}
+          {...register('description', {
+            required: 'This field is required',
+          })}
+          disabled={isCreating}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='image'>Cabin photo</Label>
-        <FileInput id='image' accept='image/*' />
+      <FormRow Label='Cabin photo'>
+        <FileInput
+          id='image'
+          accept='image/*'
+          disabled={isCreating}
+        />
       </FormRow>
 
       <FormRow>
