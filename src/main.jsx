246cd78933618b76a6a803a44e194e88c9cec768
eric ndungutse <dav.ndungutse@gmail.com>
2023-11-04 21:14:37 +0200

    add error bounding component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
 import { DarkModeProvider } from './context/DarkModeContext.jsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}>
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
